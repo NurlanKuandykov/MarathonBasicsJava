579d814b667a056c364f9e0b4d896484c8f454a4
NurlanKuandykov <nurlan_kuandykov@mail.ru>
2023-07-15 12:09:21 +0300

    Ввел случайное целое число и вывел следующее за ним четное число без использования циклов и условной функции if.

--- src/Task2.java
+++ src/Task2.java
@@ -1,5 +1,11 @@
-public class Task2 {
-    public static void main(String[] args) {
-        System.out.println("Nurik");
+import java.util.Scanner;
+
+class MyClass{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int n1 = n % 2;
+        int n2 = n - n1 + 2;
+        System.out.println(n2);
     }
 }
