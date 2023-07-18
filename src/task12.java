25aee934a49edfea473d470f120173770cd84d66
NurlanKuandykov <nurlan_kuandykov@mail.ru>
2023-07-18 12:23:09 +0300

    Вводится целое число n (0 < n < 1000). Вывести на экран сколько в нем цифр.

---
+++ src/task12.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class MyProgram{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if ((a > 9) && (a < 100)){
+            System.out.println(2);
+        }else{
+            if (a <= 9){
+                System.out.println(1);
+            }else{
+                System.out.println(3);
+            }
+        }
+    }
+}
