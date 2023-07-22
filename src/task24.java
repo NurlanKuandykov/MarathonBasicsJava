3a06ca8ebd4b91f60735a8afbd2221aa326c1117
NurlanKuandykov <nurlan_kuandykov@mail.ru>
2023-07-22 07:54:38 +0300

    Квадраты. Пользователь вводит натуральное число. Программа выводит все полные квадраты натуральных чисел, не превосходящие введенного числа.

--- src/task24.java
+++ src/task24.java
@@ -4,12 +4,12 @@ class MyClass24{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        int res = 1;
+        int res0 = 1;
         int res1 = 1;
-        while (res <= x) {
-            System.out.println(res);
+        while (res0 <= x) {
+            System.out.println(res0);
             res1++;
-            res = res1 * res1;
+            res0 = res1 * res1;
         }
     }
 }
