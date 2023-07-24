d49f90cd9c0b69efcaded1acda8ddc846cd189c1
NurlanKuandykov <nurlan_kuandykov@mail.ru>
2023-07-24 12:40:24 +0300

    Количество элементов, больших предыдущего с помощью while. Последовательность должна состоять из натуральных чисел и завершаться числом 0. Программа определяет сколько элементов этой последовательности больше предыдущего элемента. Гарантируется ввод хотя бы двух ненулевых чисел. Числа, следующие за числом 0 программа не считывает.

--- src/task12.java
+++ src/task12.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class MyProgram{
+class MyClass12{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
